cd54aa3a50faf4e695578e32dc473c9cf24e1d5b
zhafrans <52238347+zhafrans@users.noreply.github.com>
2020-04-13 15:51:33 +0700

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+console.log('Success Use Javascript')
+alert("Hallo");
+
+const subjudul = document.getElementById('subjudul');
+subjudul.innerHTML = 'Indonesia adalah negara yang indah,<em> sehingga aku sangat suka.</em>.';
+
+const navigation = document.querySelector('.navigation');
+navigation.style.backgroundColor = '#cc3300';
